e95f46860c71d30b99927bd25ddefc104a09391f
Chan Pyae Phyo Aung <114207295+chanpyaephyoaung@users.noreply.github.com>
2023-12-01 22:22:28 +0000

    Dockerise

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,12 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3001;
 
 const app = express();
 
 app.get("/", (req, res) => {
-   res.send("API is running!");
+   res.send("API is running!!");
 });
 
-app.listen(port, () => console.log(`Server running on port ${port}!`));
+app.listen(port, () => console.log(`Server running on port ${port}!!!`));
