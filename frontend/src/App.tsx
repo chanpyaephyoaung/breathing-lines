3effb33483fda396df5a2666152ff4590093569b
Chan Pyae Phyo Aung <114207295+chanpyaephyoaung@users.noreply.github.com>
2023-12-09 23:16:37 +0000

    fix eslint6

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,10 +3,10 @@ import { useState } from "react";
 
 import PoemPreviewPosts from "./components/Poems/PoemPreview/PoemPreviewPosts.tsx";
 import HeaderSection from "./components/Section/HeaderSection.tsx";
-// import SignInSection from "./components/Section/SignInSection.tsx";
-// import SignUpSection from "./components/Section/SignUpSection.tsx";
-// import UploadPoemSection from "./components/Section/UploadPoemSection.tsx";
-// import PoemFullPost from "./components/Poems/PoemFullPost/PoemFullPost.tsx";
+import SignInSection from "./components/Section/SignInSection.tsx";
+import SignUpSection from "./components/Section/SignUpSection.tsx";
+import UploadPoemSection from "./components/Section/UploadPoemSection.tsx";
+import PoemFullPost from "./components/Poems/PoemFullPost/PoemFullPost.tsx";
 import MainNav from "./components/Navbar/MainNav.tsx";
 
 const userType = "admin";
