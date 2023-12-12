99224fbb6b60da772a37f0d35d26f6543cfbfd9e
Chan Pyae Phyo Aung <114207295+chanpyaephyoaung@users.noreply.github.com>
2023-12-12 00:05:17 +0000

    Update docker compose (add env variables)

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
+import { dummyPoems } from "./data/sampleData.js";
+
 dotenv.config();
+
+console.log(process.env.NODE_ENV);
+console.log(process.env.PORT);
 
 const port = process.env.PORT || 3001;
 
@@ -8,6 +13,10 @@ const app = express();
 
 app.get("/", (req, res) => {
    res.send("API is running");
+});
+
+app.get("/api/poems", (req, res) => {
+   res.json(dummyPoems);
 });
 
 app.listen(port, () => console.log(`Server running on port ${port}!!!`));
