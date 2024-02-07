82d5997e5c37261c7fe1981a86fcead1eea8cebe
Chan Pyae Phyo Aung <114207295+chanpyaephyoaung@users.noreply.github.com>
2024-02-07 22:37:26 +0000

    Validate user email and password when signin

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema({
    name: {
@@ -77,6 +78,10 @@ const userSchema = new mongoose.Schema({
       },
    ],
 });
+
+userSchema.methods.comparePassword = async function (inputPassword) {
+   return await bcrypt.compare(inputPassword, this.password);
+};
 
 const User = mongoose.model("User", userSchema);
 
