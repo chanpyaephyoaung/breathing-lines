6868185d3fdfbecbe2f744289005866a97b05265
Chan Pyae Phyo Aung <114207295+chanpyaephyoaung@users.noreply.github.com>
2024-04-02 21:41:03 +0100

    Use collection box as navigation

--- frontend/src/components/Collection/CollectionBox.jsx
+++ frontend/src/components/Collection/CollectionBox.jsx
@@ -1,15 +1,19 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const CollectionBox = ({ collectionId, userId, name }) => {
+   const navigate = useNavigate();
+
+   const clickHandler = () => {
+      navigate(`/user-profile/${userId}/collections/${collectionId}`);
+   };
+
    return (
-      <div className="transition-all w-full max-w-[400px] mx-auto p-2 md:p-4 text-clr-black border border-1 border-clr-black cursor-pointer hover:bg-clr-primary hover:text-clr-white hover:underline">
+      <div
+         onClick={clickHandler}
+         className="transition-all w-full max-w-[400px] mx-auto p-2 md:p-4 text-clr-black border border-1 border-clr-black cursor-pointer hover:bg-clr-primary hover:text-clr-white hover:underline"
+      >
          <div className="flex items-center justify-center gap-x-4">
-            <Link
-               to={`/user-profile/${userId}/collections/${collectionId}`}
-               className="text-sm md:text-lg font-regular"
-            >
-               {name}
-            </Link>
+            <Link className="text-sm md:text-lg font-regular">{name}</Link>
          </div>
       </div>
    );
